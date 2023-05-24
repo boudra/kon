eed1a91ed3e28a3eefb841cd7cfde0ad40b64280
Mohamed Boudra <boudra.moha@gmail.com>
2023-05-24 21:06:28 +0200

    fix: konbert interface arguments

--- src/main.rs
+++ src/main.rs
@@ -8,20 +8,20 @@ use std::io::BufWriter;
 fn convert() -> Result<(), Error> {
     let args: Vec<String> = std::env::args().collect();
 
-    match args[0].as_str() {
+    match args[1].as_str() {
         "konbert" => {
-            if args.len() != 5 {
+            if args.len() != 6 {
                 return Err(Error::CustomError(
                     "invalid number of arguments".to_string(),
                 ));
             }
 
-            let input_options: ReaderOptions = serde_json::from_str(&args[2])?;
-            let reader = kon::reader::new_reader(&args[1], input_options)?;
+            let input_options: ReaderOptions = serde_json::from_str(&args[3])?;
+            let reader = kon::reader::new_reader(&args[2], input_options)?;
 
-            let output_file = File::create(&args[3])?;
+            let output_file = File::create(&args[4])?;
             let writer = BufWriter::new(output_file);
-            let output_options: WriterOptions = serde_json::from_str(&args[4])?;
+            let output_options: WriterOptions = serde_json::from_str(&args[5])?;
 
             match output_options {
                 WriterOptions::Csv(opts) => csv::writer::write(writer, reader, opts),
